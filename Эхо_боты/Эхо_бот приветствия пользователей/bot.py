7fe2768df6d4d512f42388f0f5130c2a877f2cde
Andrey Buravlev <and_bur@mail.ru>
2023-06-22 20:22:26 +0700

    Added bot menu

--- Эхо_боты/Эхо_бот приветствия пользователей/bot.py
+++ Эхо_боты/Эхо_бот приветствия пользователей/bot.py
@@ -1,5 +1,7 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types, executor
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State
 from config import TOKEN
 
 ################### ИНИЦИИРУЕМ БОТА И ДИСПЕТЧЕРА ##########################################
@@ -7,15 +9,35 @@ from config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+#################################### ИНСТРУКЦИЯ ########################################
+text='''
+Привет! Я простой эхо-бот .
+Напиши мне что-нибудь!
+<b>/start - А я буду внимательно тебя слушать и запоминать. Все что запомню - расскажу.</b>
+
+Если будет непонятно нажимай:
+<b>/help - я объясню</b>
+
+Если надоест:
+<b>/cancel - выход</b>
+'''
+
 #################################### СТАРТ ########################################
 @dp.message_handler(commands=['start'])
-async def start_command(message: types.Message):
-    await message.reply('Привет!\n Напиши мна что-нибудь!')
-
+async def start_command(message: types.Message, state: FSMContext):
+    await bot.send_message(message.chat.id, text, parse_mode='html')
+   
 #################################### HELP ########################################
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
     await message.reply('Напиши мна что-нибудь и я отправлю этот текст тебе в ответ.')
+
+# #################################### ВЫХОД ###########################################
+@dp.message_handler(commands=['cancel']) #, state= '*'
+async def cancel_command(message: types.Message, state: FSMContext): 
+    await bot.send_message(message.from_user.id, "Выход. Пока!!!\n")
+    await state.finish()
+    return False
 
 #################################### ОБРАБОТКА ТЕКСТОВОГО СООБЩЕНИЯ ########################################
 @dp.message_handler()                                       # по умолчанию библиотека делает обработку только текстовых сообщений (если скобки оставить пустыми)
